eb92a1617e70c559ac2c1d139f359dd397acce04
ArchonsSinner <nicoklinger@yahoo.de>
2017-05-30 22:09:09 +0200

    gefixt

--- src/Weka/old_helper.java
+++ src/Weka/old_helper.java
@@ -15,23 +15,25 @@ import java.io.IOException;
 
 @WebServlet("/old_helper")
 public class old_helper extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        HttpSession session = request.getSession();
-        if (session.getAttribute("uname")==null){
-            response.sendRedirect("Login");
-        }
-        session.removeAttribute("clusters");
-        if (request.getParameter("path")!= null && request.getParameter("clust")!= null){
-            session.setAttribute("file", request.getParameter("path"));
-            session.setAttribute("Clusteranzahl", request.getParameter("clust"));
-            response.getWriter().print("<link rel='stylesheet' href='bulma.css'>" +
-                    "<p>Lade " + request.getParameter("path") + " mit " + request.getParameter("clust") + " Clustern?</p>" +
-                    "<form action='StartAnalysis' method='POST'>" +
-                    "<input type='submit' class='button is-info' value='Weiter'>" +
-                    "</form>");
-        } else {
-            response.sendRedirect("Login");
-        }
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		HttpSession session = request.getSession();
+		if (session.getAttribute("uname") == null) {
+			response.sendRedirect("Login");
+		} else {
+			session.removeAttribute("clusters");
+			if (request.getParameter("path") != null && request.getParameter("clust") != null) {
+				session.setAttribute("file", request.getParameter("path"));
+				session.setAttribute("Clusteranzahl", request.getParameter("clust"));
+				response.getWriter()
+						.print("<link rel='stylesheet' href='bulma.css'>" + "<p>Lade " + request.getParameter("path")
+								+ " mit " + request.getParameter("clust") + " Clustern?</p>"
+								+ "<form action='StartAnalysis' method='POST'>"
+								+ "<input type='submit' class='button is-info' value='Weiter'>" + "</form>");
+			} else {
+				response.sendRedirect("Login");
+			}
+		}
+	}
 }
